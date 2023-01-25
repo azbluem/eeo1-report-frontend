ea77cad8a3f028f02b8420ea0f380bd74d4271fc
Margaret <y.margaret.lu+github@gmail.com>
2023-01-24 16:36:35 -0800

    able to make trial data in chart work

--- eeo1-frontend/src/App.js
+++ eeo1-frontend/src/App.js
@@ -1,17 +1,42 @@
 import Caption from "./components/Caption"
-import Chart from "./components/Chart"
+import Graph from "./components/Chart"
 import Hero from "./components/Hero"
 import Menu from "./components/Menu"
 import Selector from "./components/Selector"
+import DATA from './DATA'
+import {Chart, ArcElement,Tooltip, Legend} from 'chart.js'
+
+import './App.css'
+import { useState } from "react"
+
+Chart.register(ArcElement,Tooltip, Legend);
 
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: DATA.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: DATA.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
   return (
-    <div>
-      <Menu/>
-      <Hero/>
-      <Selector/>
-      <Chart/>
-      <Caption/>
+    <div className="body">
+      <Menu className='menu'/>
+      <Hero className='hero'/>
+      <Selector className='selector'/>
+      <Graph className='chart' chartData={chartData}/>
+      <Caption className='caption'/>
     </div>
   );
 }
--- eeo1-frontend/src/components/Chart.js
+++ eeo1-frontend/src/components/Chart.js
@@ -1,4 +1,23 @@
-const Chart = (props) => {
-    return <div>This is the Chart</div>
+import React from "react";
+import { Pie } from "react-chartjs-2";
+
+function Graph({ chartData }) {
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Pie Chart</h2>
+      <Pie
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            }
+          }
+        }}
+      />
+    </div>
+  );
 }
-export default Chart
+
+export default Graph

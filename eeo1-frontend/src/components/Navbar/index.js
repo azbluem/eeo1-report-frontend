ad6237892e9d386e164bca124da15b06a317158e
Margaret <y.margaret.lu+github@gmail.com>
2023-01-26 14:31:55 -0800

    formatted NavBar

--- eeo1-frontend/src/components/Navbar/index.js
+++ eeo1-frontend/src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import {
   Nav,
   NavLink,
@@ -7,11 +7,20 @@ import {
 } from './NavbarElements';
   
 const Navbar = () => {
+  const Sidebar = () => {
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+  }
+  const [width,setWidth] = useState(window.innerWidth)
+  if (width > 768) {
+    return
+  }
+
+
   return (
     <>
       <Nav>
-        <Bars />
-  
+        {/* <Bars /> */}
         <NavMenu>
         <NavLink to='/eeo1-report-frontend' activestyle='true'>
             Home

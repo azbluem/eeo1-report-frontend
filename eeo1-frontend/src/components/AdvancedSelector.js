37599c3991d46af58cca639a61785e36bdb3cdf3
Margaret <y.margaret.lu+github@gmail.com>
2023-02-02 17:04:07 -0800

    boilerplate for advanced mode

---
+++ eeo1-frontend/src/components/AdvancedSelector.js
@@ -0,0 +1,21 @@
+import CompanyDropDown from './CompanyDropDown'
+import SortDropDown from './SortDropDown'
+import YearDropDown from './YearDropDown'
+
+// const COMPANIES = ['Amazon','Manga','all']
+
+const AdvancedSelector = ({companyList, yearList, params,setQueryParams,getOneCompanyData}) => {
+    const setParams = (e) => {
+        const newParams = {
+            ...params,
+            [e.target.name] : e.target.value
+        }
+        setQueryParams(newParams);
+        getOneCompanyData(newParams);
+    };
+    
+    return (<div><CompanyDropDown params={params} setParams={setParams} companyList={companyList}/> 
+    by <SortDropDown params={params} setParams={setParams}/> 
+    for <YearDropDown params={params} setParams={setParams} companyList={companyList}/></div>)
+}
+export default AdvancedSelector
